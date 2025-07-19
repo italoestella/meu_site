82baedd099d65a565d6c4be6f933afc114de48ec
Italo Estella <italo.estella@kovex.ind.br>
2025-07-19 14:16:09 -0300

    Adiciona gunicorn ao requirements.txt

--- app.py
+++ app.py
@@ -8,5 +8,8 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    print(f"Rodando na porta: {port}")
+    app.run(host="0.0.0.0", port=port, debug=True)
+
